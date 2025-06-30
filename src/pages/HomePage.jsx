b16e32be7d110c7c9ac086e8dbee72f7c42a6859
sirjoaogoncalves <joaogon85@gmail.com>
2025-06-30 18:40:48 +0100

    testimonials not needed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -41,15 +41,6 @@ const HomePage = () => {
         viewport={{ once: true }}
         transition={{ duration: 0.8 }}
       >
-        <TestimonialsSection />
-      </motion.div>
-      
-      <motion.div
-        initial={{ opacity: 0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.8 }}
-      >
         <CtaSection />
       </motion.div>
     </div>
